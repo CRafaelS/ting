201759a0370f11778bd829d3c269537d41fdafd9
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-10-24 16:57:01 -0300

    feat: Req 2 em andamento

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,9 @@
+import sys
+import pathlib
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file:
+        print(f"Arquivo {path_file} não encontrado\n", file=sys.stderr)
+    if (pathlib.Path(path_file).suffix == ".txt"):
+        print("Formato inválido\n", file=sys.stderr)
