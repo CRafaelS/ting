35de644887d01ab81e109990da8785e5bd07bebb
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-10-24 16:24:42 -0300

    feat: Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,24 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
+
+    def is_empty(self):
+        return not bool(self.__len__)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.is_empty():
+            return None
+        value = self._data[0]
+        del self._data[0]
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self._data):
+            return self._data[index]
+        raise IndexError
