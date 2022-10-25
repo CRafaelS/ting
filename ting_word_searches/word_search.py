fdb69e3a26ff9e87b7aab63ab3dea16c66e90588
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-10-25 16:17:52 -0300

    req 6 e 7, ok

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,35 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_lower = word.lower()
+    result = []
+    for index in instance._data:
+        base_structure = {
+            "palavra": word,
+            "arquivo": index["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for i, words in enumerate(index["linhas_do_arquivo"]):
+            if word_lower in words.lower():
+                base_structure["ocorrencias"].append({"linha": i + 1})
+        if len(base_structure["ocorrencias"]) > 0:
+            result.append(base_structure)
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_lower = word.lower()
+    result = []
+    for index in instance._data:
+        base_structure = {
+            "palavra": word,
+            "arquivo": index["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for i, words in enumerate(index["linhas_do_arquivo"]):
+            if word_lower in words.lower():
+                base_structure["ocorrencias"].append({
+                        "linha": i + 1,
+                        "conteudo": words
+                        })
+        if len(base_structure["ocorrencias"]) > 0:
+            result.append(base_structure)
+    return result
